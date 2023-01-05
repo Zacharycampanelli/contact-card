a2fd5b22ce991ea4e46d923d16cab9bcf6d315e1
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-01-05 16:26:54 -0500

    Configure the application to use the IndexedDB API

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,8 @@
 import  "./form";
 import "./submit";
 
+import { initdb } from "./database" 
+
 import "../css/index.css"
 
 import { Tooltip, Toast, Popover } from 'bootstrap';
@@ -11,6 +13,7 @@ import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
